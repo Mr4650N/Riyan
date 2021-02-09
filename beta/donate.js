9e0512dcbdd41170261368c1766370e24024e80f
Mr4650N <78628140+Mr4650N@users.noreply.github.com>
2021-02-10 05:36:33 +0700

    Add files via upload

---
+++ beta/donate.js
@@ -0,0 +1,28 @@
+exports.donate = (id, XBOT, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) => {
+	return `
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+╔════════════════════
+║ *Donasi Ke MIMIN X BOT*
+╠════════════════════
+║├≽️⚜ *PULSA* : _0898-9031-500_
+║├≽️⚜ *DANA* : _0898-9031-500_
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Facebook <Follow>*
+║│ _PerusakGame/Ibnu.ismoyo_
+║│2. *Creator MIMIN X BOT*
+║│ _wa.me/628989031500_
+║╰───────────
+╠════════════════════
+║ _*MADE BY INR21*_
+╠════════════════════
+║ゞ📆 *${tampilTanggal}*
+║ゞ⏱️ *${tampilWaktu}*
+║ゞ📢 Bot Aktif ; *${aktif}*
+╚════════════════════`
+}
+
---
+++ beta/info.js
@@ -0,0 +1,30 @@
+exports.info = (id, XBOT, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `*MENU ${XBOT}*
+  
+❉──────────❉
+  Hai. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+╔════════════════════
+║ *INFO MIMIN X BOT*
+╠════════════════════
+║├≽️⚜ *AUTOR* : _IBNU NR_
+║├≽️⚜ *DESIGNER* : _IBNU NR_
+║├≽️⚜ *OWNER* : _IBNU NR_
+╠════════════════════
+║  *MIMIN X BOT*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Facebook <Follow>*
+║│ _PerusakGame/Ibnu.ismoyo_
+║│4. *Creator MIMIN X BOT*
+║│ _wa.me/628989031500_
+║╰───────────
+╠════════════════════
+║ _*MADE BY INR21*_
+╠════════════════════
+║ゞ📆 *${tampilTanggal}*
+║ゞ⏱️ *${tampilWaktu}*
+║ゞ📢 Bot Aktif ; *${aktif}*
+╚════════════════════`
+}
